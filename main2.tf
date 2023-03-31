731c6f6a73f231ccae83ba6ad306397f2d26021d
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-03-31 18:55:30 -0400

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -1,7 +1,6 @@
-# arunipterra
-#readme
 #create a External IP
-provider="google"
-resource="google_compute_global_address""default" {
-name="global-server-ip"
+provider "google" {}
+resource "google_compute_global_address""default"{
+  name="global-server-ip"
   project="mcit-arun"
+}
